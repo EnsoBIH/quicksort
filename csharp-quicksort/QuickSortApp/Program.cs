b7e22b03ee282ce6c3c4d93835ce43f41d26df0f
ensobosna <147910441+EnsoBIH@users.noreply.github.com>
2024-11-04 10:59:37 +0100

    add csharp quicksort

---
+++ csharp-quicksort/QuickSortApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var arr = new int[] { 10, 7, 8, 9, 1, 5 };
+        quickSort(arr, 0, arr.Length - 1);
+        Console.WriteLine("Sorted array: ");
+        foreach (var item in arr)
+        {
+            Console.Write(item + " ");
+        }
+    }
+
+    public static void quickSort(int[] arr, int low, int high)
+    {
+        int left = low;
+        int right = high;
+        int pivot = arr[(low + high) / 2];
+
+        while (left <= right) {
+            while (arr[left] < pivot) {
+                left++;
+            }
+            while (arr[right] > pivot) {
+                right--;
+            }
+            if (left <= right) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+
+        if (low < right) {
+            quickSort(arr, low, right);
+        }
+        if (left < high) {
+            quickSort(arr, left, high);
+        }
+    }
+}
